a1ca644c872ce241be8b1d54c9cbf8368223c127
Thanh39 <dinhthanh392000@gmail.com>
2022-07-29 09:29:20 +0700

    use route to exchange page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+function DetailMeetup() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg/341px-Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg"
+      title="A second Meetup"
+      address="Some address 5, 1243 city"
+      description="this A second MEetup"
+    ></MeetupDetail>
+  );
+}
+export default DetailMeetup;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,30 @@
-import MeetupList   from '../components/meetups/MeetupList'
+import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
+import { useEffect, useState } from "react";
 const DUMY_MEETUPS = [
-    {
-        id:'m1',
-        title:'A first Meetup',
-        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg/341px-Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg',
-        address:'Some address 5, 1243 city',
-        description:'this A first MEetup'
-    },
-    {
-        id:'m2',
-        title:'A second Meetup',
-        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg/341px-Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg',
-        address:'Some address 5, 1243 city',
-        description:'this A second MEetup'
-    },
-]
-function HomePage(){
-    return <MeetupList meetups={DUMY_MEETUPS}/>
+  {
+    id: "m1",
+    title: "A first Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg/341px-Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg",
+    address: "Some address 5, 1243 city",
+    description: "this A first MEetup",
+  },
+  {
+    id: "m2",
+    title: "A second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg/341px-Convex_lens_%28magnifying_glass%29_and_upside-down_image.jpg",
+    address: "Some address 5, 1243 city",
+    description: "this A second MEetup",
+  },
+];
+function HomePage() {
+  const [loadMeetup, setLoadMeetup] = useState([]);
+  useEffect(
+    //render one times
+    setLoadMeetup(DUMY_MEETUPS)
+  , []);
+  return <MeetupList meetups={loadMeetup} />;
 }
 export default HomePage;
